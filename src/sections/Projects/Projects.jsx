c06231e65226f2e60e68826ef09aa7db3f46deca
Hamza <hamzahaboubi2003@gmail.com>
2024-11-02 14:33:23 +0000

    made some few changes

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,8 +1,8 @@
 import styles from './ProjectsStyles.module.css';
-import viberr from '../../assets/viberr.png';
-import freshBurger from '../../assets/fresh-burger.png';
-import hipsster from '../../assets/hipsster.png';
-import fitLift from '../../assets/fitlift.png';
+import weatheryy from '../../assets/weatheryy.png';
+import l0ck3r from '../../assets/l0ck3r.png';
+import portfolio from '../../assets/portfolio.png';
+import calculator from '../../assets/calculator.png';
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -11,26 +11,26 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
-          link="https://github.com/Ade-mir/company-landing-page-2"
+          src={weatheryy}
+          link="https://github.com/h1mzaHb/weather-app-js"
           h3="Weatheryy"
           p="weather forecast"
         />
         <ProjectCard
-          src={freshBurger}
-          link="https://github.com/Ade-mir/company-landing-page-2"
+          src={l0ck3r}
+          link="https://github.com/h1mzaHb/Password-generator"
           h3="l0ck3r"
           p="Password generator"
         />
         <ProjectCard
-          src={hipsster}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Fresh pizza"
-          p="Pizza restaurant website"
+          src={portfolio}
+          link="https://github.com/h1mzaHb/portfolio"
+          h3="Portfolio"
+          p="My React portfolio"
         />
         <ProjectCard
-          src={fitLift}
-          link="https://github.com/Ade-mir/company-landing-page-2"
+          src={calculator}
+          link="https://github.com/h1mzaHb/calculator"
           h3="Calculator"
           p="Advanced calculator app"
         />
