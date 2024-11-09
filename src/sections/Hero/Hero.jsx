644c01a4e2b00fbd61403c4691431d0460f39d1b
h1mzaHb <hamzahaboubi2003@gmail.com>
2024-11-09 03:12:58 +0100

    adjusted animation

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -20,7 +20,7 @@ function Hero() {
   return (
     <section id="hero" className={styles.container}>
       <motion.div
-      variants={fadeIn("up", 0.2)}
+      variants={fadeIn("left", 0.3)}
       initial="hidden"
       whileInView={"show"}
       viewport={{ once: true, amount: 0.7 }}
@@ -38,10 +38,11 @@ function Hero() {
         />
       </motion.div>
       <motion.div 
-      variants={fadeIn("up", 0.2)}
+      variants={fadeIn("right", 0.3)}
       initial="hidden"
       whileInView={"show"}
-      viewport={{ once: true, amount: 0.7 }}className={styles.info}>
+      viewport={{ once: true, amount: 0.7 }}
+      className={styles.info}>
         <h1>
           Hamza
           <br />
